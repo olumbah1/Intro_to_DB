99b49a342ad2b67a26d7dd8a07273fee1008a30a
Olumide <adeoyeolumbah@gmail.com>
2025-06-26 16:36:23 +0100

    Creating tables inside the database

---
+++ task_2.sql
@@ -0,0 +1,32 @@
+CREATE TABLE Books (
+book_id INT AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(130),
+author_id FOREIGN KEY REFERENCES Authors TABLE,
+price DOUBLE,
+publication_date DATE
+);
+
+CREATE TABLE Authors (
+author_id PRIMARY KEY,
+author_name VARCHAR(215)
+);
+
+CREATE TABLE Customers (
+customer_id PRIMARY KEY,
+customer_name VARCHAR(215),
+email VARCHAR(215),
+address TEXT
+);
+
+CREATE TABLE Orders(
+order_id INT PRIMARY KEY,
+customer_id INT FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+order_date DATE
+);
+
+CREATE TABLE Order_Details(
+orderdetailid PRIMARY KEY,
+order_id FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+book_id FOREIGN KEY (book_id) REFERENCES Books(book_id),
+quantity DOUBLE
+);
