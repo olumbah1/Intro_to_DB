b5796645904a80c828cdd8c874da66e479059044
Olumide <adeoyeolumbah@gmail.com>
2025-06-26 12:17:46 +0100

    A database schema how data are stored on the database

---
+++ alx_book_store.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE alx_book_store;
+CREATE TABLE Books (
+book_id INT AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(130),
+author_id FOREIGN KEY REFERENCES Authors TABLE,
+price DOUBLE,
+publication_date DATE
+);
+
+CREATE TABLE Authors (
+author_id PRIMARY KEY,
+author_name VARCHAR(215)
+);
+
+CREATE TABLE Customers (
+customer_id PRIMARY KEY,
+customer_name VARCHAR(215),
+email VARCHAR(215),
+address VARCHAR(215)
+);
+
+CREATE TABLE Orders(
+order_id PRIMARY KEY,
+customer_id FOREIGN KEY REFERENCES Customers TABLE,
+order_date DATE
+);
+
+CREATE TABLE Order_Details(
+orderdetailid PRIMARY KEY,
+order_id FOREIGN KEY REFERENCES Orders TABLE,
+book_id FOREIGN KEY REFERENCES Books TABLE,
+quantity DOUBLE
+);
