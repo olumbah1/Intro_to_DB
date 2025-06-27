0c38b4f946cc3c4bfff92cdb297622360d9c2110
Olumide <adeoyeolumbah@gmail.com>
2025-06-27 12:41:33 +0100

    Added line of command that insert values

--- task_5.sql
+++ task_5.sql
@@ -1,9 +1,9 @@
-CREATE TABLE Customer (
+CREATE TABLE customer (
 customer_id PRIMARY KEY,
 customer_name VARCHAR(215),
 email VARCHAR(215),
 address TEXT
 );
 
-INSERT INTO Customer (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES(1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Hapiness Ave.');
