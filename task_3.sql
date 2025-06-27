87647838e4881a68bf8d6579e010c35a89d057b3
Olumide <adeoyeolumbah@gmail.com>
2025-06-27 13:31:56 +0100

    imputing values into customer table

--- task_3.sql
+++ task_3.sql
@@ -1,3 +1,6 @@
 USE alx_book_store;
 SHOW TABLES;
-        
+       (customer_id=2, customer_name='Blessing Malik', email='bmalik@sandtech.com', address='124 Happiness  Ave.'
+ customer_id=3, customer_name='Obed Ehoneah', email='eobed@sandtech.com', address='125 Happiness  Ave.'
+ customer_id=4, customer_name='Blessing Malik', email='nkamolu@sandtech.com', address='126 Happiness  Ave.'
+); 
