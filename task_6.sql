2e1700b7c232c7dce9d3d2ca3115fd745f41caa7
Olumide <adeoyeolumbah@gmail.com>
2025-06-27 13:10:19 +0100

    Imputing values into customer table

--- task_6.sql
+++ task_6.sql
@@ -5,9 +5,8 @@ email VARCHAR(215),
 address TEXT
 );
 
-INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES
-(1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.'
- 2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.' 
- 3, 'Obed Ehoneah','eobed@sandtech.com', '125 Happiness Ave.'
- 4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave');
+INSERT INTO customer VALUES
+(customer_id = 2, customer_name = 'Blessing Malik', email = 'bmalik@sandtech.com', address = '124 Happiness Ave.'
+ customer_id = 3, customer_name = 'Obed Ehoneah', email = 'eobed@sandtech.com', address = '125 Happiness Ave.'
+ customer_id = 4, customer_name = 'Blessing Malik', email = 'nkamolu@sandtech.com', address = '126 Happiness Ave.'
+);
