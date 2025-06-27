9dcc00e92bd9f0e38ec09cdd7fba71d2dd7ef7b7
Olumide <adeoyeolumbah@gmail.com>
2025-06-27 12:43:16 +0100

    Added line of command that insert values

--- task_5.sql
+++ task_5.sql
@@ -6,4 +6,4 @@ address TEXT
 );
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES(1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Hapiness Ave.');
+VALUES(1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
