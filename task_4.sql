98a68a1366f48cf0773fe88b82b054c7f39d3390
Olumide <adeoyeolumbah@gmail.com>
2025-06-30 15:55:08 +0100

    Made changes

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,3 @@
--- The database name will be passed as an argument of the mysql command
--- You are not allowed to use the DESCRIBE or EXPLAIN statements
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE  TABLE_SCHEMA = 'alx_book_store'
